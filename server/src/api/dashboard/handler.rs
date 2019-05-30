f9ab0c459f28cf66fca444391724c2cc264da3a2
Daniel Holzner <danielholzner@gmx.de>
2019-05-30 14:59:10 +0200

    Refactor: Remove actors/handlers part 1

--- server/src/api/dashboard/handler.rs
+++ server/src/api/dashboard/handler.rs
@@ -1,83 +1,14 @@
-use crate::api::auth::handler::LoggedUser;
-use crate::errors::ServiceError;
-use crate::models::{Dashboard, DbExecutor};
-use actix::{Handler, Message};
+use crate::models::Dashboard;
 use serde_json::Value;
-
-#[derive(Debug, Deserialize)]
-pub struct GetDashboards {
-    pub username: String,
-}
+use database::ConnectionPool;
+use actix_web::web;
+use diesel::result::Error;
 
 #[derive(Debug, Serialize)]
 pub struct DashboardMeta {
     pub id: i32,
     pub name: String,
     pub is_default: bool,
-}
-
-impl Message for GetDashboards {
-    type Result = Result<Vec<DashboardMeta>, ServiceError>;
-}
-
-impl From<LoggedUser> for GetDashboards {
-    fn from(logged_user: LoggedUser) -> Self {
-        GetDashboards {
-            username: logged_user.username,
-        }
-    }
-}
-
-impl Handler<GetDashboards> for DbExecutor {
-    type Result = Result<Vec<DashboardMeta>, ServiceError>;
-    fn handle(&mut self, msg: GetDashboards, _: &mut Self::Context) -> Self::Result {
-        self.0
-            .get_user(&msg.username)
-            .and_then(|user| self.0.get_dashboards_for_user(&user))
-            .and_then(|dashboards| {
-                Ok(dashboards
-                    .iter()
-                    .map(|x| DashboardMeta {
-                        id: x.0,
-                        name: x.1.to_owned(),
-                        is_default: x.2,
-                    })
-                    .collect())
-            })
-            .map_err(|_e| ServiceError::InternalServerError)
-    }
-}
-
-#[derive(Debug, Deserialize)]
-pub struct DashboardData {
-    pub id: i32,
-}
-
-pub struct GetDashboard {
-    pub username: String,
-    pub dashboard_id: i32,
-}
-
-impl Message for GetDashboard {
-    type Result = Result<Dashboard, ServiceError>;
-}
-
-impl Handler<GetDashboard> for DbExecutor {
-    type Result = Result<Dashboard, ServiceError>;
-    fn handle(&mut self, msg: GetDashboard, _: &mut Self::Context) -> Self::Result {
-        self.0
-            .get_user(&msg.username)
-            .and_then(|user| self.0.get_dashboard_for_user(msg.dashboard_id, &user))
-            .map_err(|_e| ServiceError::InternalServerError)
-    }
-}
-
-pub struct GetDefaultDashboard {
-    pub username: String,
-}
-
-impl Message for GetDefaultDashboard {
-    type Result = Result<DashboardMeta, ServiceError>;
 }
 
 impl From<Dashboard> for DashboardMeta {
@@ -89,24 +20,39 @@ impl From<Dashboard> for DashboardMeta {
         }
     }
 }
- 
 
-impl Handler<GetDefaultDashboard> for DbExecutor {
-    type Result = Result<DashboardMeta, ServiceError>;
-    fn handle(&mut self, msg: GetDefaultDashboard, _: &mut Self::Context) -> Self::Result {
-        self.0
-            .get_user(&msg.username)
-            .and_then(|user| self.0.get_default_dashboard_for_user(&user))
-            .and_then(|dashboard| Ok(dashboard.into()))
-            .map_err(|_e| ServiceError::InternalServerError)
-    }
+pub fn get_dashboards_for_user(pool: web::Data<ConnectionPool>, username: &str) -> Result<Vec<DashboardMeta>, Error> {
+    pool            
+        .get_user(username)
+        .and_then(|user| pool.get_dashboards_for_user(&user))
+        .and_then(|dashboards| {
+            Ok(dashboards
+                .iter()
+                .map(|x| DashboardMeta {
+                    id: x.0,
+                    name: x.1.to_owned(),
+                    is_default: x.2,
+                })
+                .collect())
+        })
 }
 
-#[derive(Debug)]
-pub struct SaveDashboard {
-    pub username: String,
+#[derive(Debug, Deserialize)]
+pub struct DashboardData {
     pub id: i32,
-    pub new_settings: Value,
+}
+
+pub fn get_dashboard_by_id(pool: web::Data<ConnectionPool>, dashboard_id: i32, username: &str) -> Result<Dashboard, Error> {
+    pool
+        .get_user(username)
+        .and_then(|user| pool.get_dashboard_for_user(dashboard_id, &user))
+}
+
+pub fn get_default_dashboard_for_user(pool: web::Data<ConnectionPool>, username: &str) -> Result<DashboardMeta, Error> {
+    pool
+        .get_user(username)
+        .and_then(|user| pool.get_default_dashboard_for_user(&user))
+        .and_then(|dashboard| Ok(dashboard.into()))
 }
 
 #[derive(Debug, Deserialize)]
@@ -115,16 +61,8 @@ pub struct DashboardSettings {
     pub settings: Value,
 }
 
-impl Message for SaveDashboard {
-    type Result = Result<DashboardMeta, ServiceError>;
-}
-
-impl Handler<SaveDashboard> for DbExecutor {
-    type Result = Result<DashboardMeta, ServiceError>;
-    fn handle(&mut self, msg: SaveDashboard, _: &mut Self::Context) -> Self::Result {
-        self.0
-            .get_user(&msg.username)
-            .and_then(|user| Ok(self.0.save_dashboard_for_user(&user, msg.id, &msg.new_settings).into()))
-            .map_err(|_e| ServiceError::InternalServerError)
-    }
+pub fn save_dashboard_for_user(pool: web::Data<ConnectionPool>, msg: &DashboardSettings, username: &str) -> Result<DashboardMeta, Error> {
+    pool
+        .get_user(username)
+        .and_then(|user| Ok(pool.save_dashboard_for_user(&user, msg.id, &msg.settings).into()))
 }
--- server/src/api/dashboard/routes.rs
+++ server/src/api/dashboard/routes.rs
@@ -1,33 +1,31 @@
-use super::handler::{DashboardData, GetDashboard, GetDashboards, GetDefaultDashboard, SaveDashboard, DashboardSettings};
+use super::handler::{DashboardData, DashboardSettings};
+use super::handler::{
+    get_dashboards_for_user,
+    get_dashboard_by_id,
+    get_default_dashboard_for_user,
+    save_dashboard_for_user,
+};
 use crate::api::auth::handler::LoggedUser;
-use crate::models::DbExecutor;
-use actix::Addr;
 use actix_web::{web, Error, HttpResponse, ResponseError};
 use futures::Future;
+use database::ConnectionPool;
 
 pub fn get_dashboards(
     logged_user: LoggedUser,
-    db: web::Data<Addr<DbExecutor>>,
+    pool: web::Data<ConnectionPool>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
-    db.send(GetDashboards::from(logged_user))
-        .from_err()
-        .and_then(move |res| match res {
-            Ok(dashboards) => Ok(HttpResponse::Ok().json(dashboards)),
-            Err(e) => Ok(e.error_response()),
-        })
+   web::block(move || get_dashboards_for_user(pool, &logged_user.username)).then(|res| match res {
+        Ok(dashboards) => Ok(HttpResponse::Ok().json(dashboards)),
+        Err(e) => Ok(e.error_response()),
+    })
 }
 
 pub fn get_dashboard(
     logged_user: LoggedUser,
     dashboard_data: web::Json<DashboardData>,
-    db: web::Data<Addr<DbExecutor>>,
+    pool: web::Data<ConnectionPool>
 ) -> impl Future<Item = HttpResponse, Error = Error> {
-    db.send(GetDashboard {
-        username: logged_user.username,
-        dashboard_id: dashboard_data.id,
-    })
-    .from_err()
-    .and_then(move |res| match res {
+    web::block(move || get_dashboard_by_id(pool, dashboard_data.id, &logged_user.username)).then(|res| match res {
         Ok(dashboard) => Ok(HttpResponse::Ok().json(dashboard)),
         Err(e) => Ok(e.error_response()),
     })
@@ -35,13 +33,9 @@ pub fn get_dashboard(
 
 pub fn get_default_dashboard(
     logged_user: LoggedUser,
-    db: web::Data<Addr<DbExecutor>>,
+    pool: web::Data<ConnectionPool>
 ) -> impl Future<Item = HttpResponse, Error = Error> {
-    db.send(GetDefaultDashboard {
-        username: logged_user.username,
-    })
-    .from_err()
-    .and_then(move |res| match res {
+    web::block(move || get_default_dashboard_for_user(pool, &logged_user.username)).then(|res| match res {
         Ok(dashboard) => Ok(HttpResponse::Ok().json(dashboard)),
         Err(e) => Ok(e.error_response()),
     })
@@ -50,15 +44,9 @@ pub fn get_default_dashboard(
 pub fn save_dashboard(
     logged_user: LoggedUser,
     dashboard_data: web::Json<DashboardSettings>,
-    db: web::Data<Addr<DbExecutor>>,
+    pool: web::Data<ConnectionPool>
 ) -> impl Future<Item = HttpResponse, Error = Error> {
-     db.send(SaveDashboard {
-         username: logged_user.username,
-         id: dashboard_data.id,
-         new_settings: dashboard_data.settings.clone(),
-     })
-    .from_err()
-    .and_then(move |res| match res {
+     web::block(move || save_dashboard_for_user(pool, &dashboard_data.into_inner(), &logged_user.username)).then(|res| match res {
         Ok(_) => Ok(HttpResponse::Ok().into()),
         Err(e) => Ok(e.error_response()),
     })
